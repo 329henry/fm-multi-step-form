43606552154bf1a80d29b7d09b161a2e2eb86459
329henry <tksssp329@gmail.com>
2025-08-24 00:56:55 +0900

    fix: fix font family, update folder hierarchy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import PlanButton from '@components/planButton/planButton'
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center bg-slate-100 min-h-screen">
       <div className="bg-white p-4 rounded-lg w-[940px] h-[600px]">
         <Image className="" src="/bg-sidebar-desktop.svg" alt="Desktop banner" width={274} height={568} priority />
+        <PlanButton plan={{ name: 'Arcade', price: '$9/mo' }} />
       </div>
     </div>
   )
