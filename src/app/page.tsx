c6c07d711fc34bfb702412af6a3b3617070fe44c
329henry <tksssp329@gmail.com>
2025-08-24 23:43:48 +0900

    feat: add step 1 and 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
-import PlanButton from '@components/planButton/planButton'
+import YourInfo from '@pages/yourInfo/yourInfo'
+import PlanSelect from './pages/planSelect/planSelect'
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center bg-slate-100 min-h-screen">
-      <div className="bg-white p-4 rounded-lg w-[940px] h-[600px]">
+      <div className="flex bg-white p-4 rounded-lg w-[940px] h-[600px]">
         <Image className="" src="/bg-sidebar-desktop.svg" alt="Desktop banner" width={274} height={568} priority />
-        <PlanButton plan={{ name: 'Arcade', price: '$9/mo' }} />
+        {/* <YourInfo /> */}
+        <PlanSelect />
       </div>
     </div>
   )
---
+++ src/app/pages/yourInfo/yourInfo.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+import Title from '@/app/components/title/title'
+
+type Input = {
+  name: string
+  email: string
+  phone: string
+}
+
+export default function YourInfo() {
+  const {
+    register,
+    handleSubmit
+    // formState: { errors }
+  } = useForm<Input>()
+
+  return (
+    <div className="flex flex-col h-full p-6">
+      <Title name="YOUR_INFO" />
+      <form
+        className="flex flex-col justify-between gap-4 mt-6 flex-grow"
+        onSubmit={handleSubmit((data) => console.log(data))}
+      >
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name" className="text-denim">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder="e.g. Stephen King"
+              className="border border-grey rounded-md p-2 text-black"
+              {...register('name', { required: true })}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="text-denim">
+              Email Address
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="e.g. stephenking@lorem.com"
+              className="border border-grey rounded-md p-2 text-black"
+              {...register('email', { required: true })}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="phone" className="text-denim">
+              Phone Number
+            </label>
+            <input
+              id="phone"
+              type="tel"
+              placeholder="e.g. +1 234 567 890"
+              className="border border-grey rounded-md p-2 text-black"
+              {...register('phone', { required: true })}
+            />
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <button type="submit" className="h-[48px] w-[123px] bg-denim rounded-lg text-white">
+            Next Step
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
