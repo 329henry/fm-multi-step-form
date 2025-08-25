46ebff98ef6a0583726e170dd64dc0e7fd6f283e
329henry <tksssp329@gmail.com>
2025-08-26 01:10:56 +0900

    fix: replace Image with navigationList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import Image from 'next/image'
 import YourInfo from '@pages/yourInfo/yourInfo'
-import PlanSelect from './pages/planSelect/planSelect'
+import PlanSelect from '@pages/planSelect/planSelect'
+import NavigationList from './components/navigationList/navigationList'
+import Addons from '@pages/addons/addons'
+import Summary from '@pages/summary/summary'
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center bg-slate-100 min-h-screen">
       <div className="flex bg-white p-4 rounded-lg w-[940px] h-[600px]">
-        <Image className="" src="/bg-sidebar-desktop.svg" alt="Desktop banner" width={274} height={568} priority />
+        <NavigationList />
         {/* <YourInfo /> */}
-        <PlanSelect />
+        {/* <PlanSelect /> */}
+        {/* <Addons /> */}
+        <Summary />
       </div>
     </div>
   )
